b23ff12bf7dbadf227b3c4980a3585f21270a6bd
Boniface <cheruiyotbony@gmail.com>
2024-04-13 19:34:54 +0300

    done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,35 +15,31 @@ import downloadIcon from "../../assets/icons/download-icon.svg";
 const About = () => {
   return (
     <StyledAbout id="about">
-      <SectionHeading dark="true" mb="3rem">
-        <SectionTitle dark="true">About Me</SectionTitle>
+      <SectionHeading dark={true} mb="3rem">
+        <SectionTitle dark={true}>About Me</SectionTitle>
       </SectionHeading>
 
       <AboutContent>
-        <AboutImage src={myImage} />
+        <AboutImage src={myImage} alt="Omkar Londhe" />
 
         <AboutDetailsContainer>
           <StyledParagraph>
-           Over the last 5 years, my passion for IoT and its underlying technology has been a driving force. 
-           This journey naturally led me to explore the captivating world of software engineering. 
-
+            Over the last 5 years, I've immersed myself in the world of software engineering, honing my skills in Python, Flask, and React by building numerous projects.
           </StyledParagraph>
           <StyledParagraph>
-            Though I am an amateur in this new world, I always  knew that I wanted to
-            pursue my goals in this field.  I am
-            skilled in handling HTML, CSS, JavaScript, node.js, Python, React,
-            SQL and GitHub. I'm always enthusiastic to learn new things and
-            highly passionate about building web applications besides data analytics
+            My journey began with a fascination for IoT and its technology, which naturally led me to explore software engineering. Since then, I've dedicated myself to mastering various technologies, including HTML, CSS, JavaScript, node.js, and SQL.
           </StyledParagraph>
           <StyledParagraph>
-            In my free time, I explore the world around me through a combination
-            of hiking and exploring new swimming pools .
+            I thrive on challenges and am always eager to learn new things. Whether it's crafting elegant code or analyzing complex data, I approach every task with enthusiasm and dedication.
+          </StyledParagraph>
+          <StyledParagraph>
+            Outside of coding, you'll find me exploring the outdoors, whether it's hiking through rugged terrain or discovering new swimming spots.
           </StyledParagraph>
           <Resume>
             <ResumeLink
               download="Omkar_Londhe"
               href="https://docs.google.com/document/d/1FXr_W357unjJWkWfqHjItycav2acddK8mFXTDcbBnmc/edit?usp=sharing"
-              target={"_blank"}
+              target="_blank"
               icon={downloadIcon}
             >
               View Resume
