e95381ce06f74e4045ec827ce6513d527917cdf3
Boniface <cheruiyotbony@gmail.com>
2024-06-29 22:52:44 +0300

    done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,95 @@
 import React from "react";
+import styled from "styled-components";
 import { animateScroll as scroll } from "react-scroll";
-import {
-  ArrowToTop,
-  ContactHeader,
-  ContactLink,
-  FooterBigLink,
-  FooterBigLinkContainer,
-  FooterContact,
-  FooterContacts,
-  FooterLine,
-  FooterSmallTitle,
-  FooterSmallTitleWrapper,
-  FooterSocials,
-  StyledFooter,
-} from "../styles/Footer.styled";
 import { SiLinkedin, SiGithub, SiTwitter } from "react-icons/si";
-import { BsFillArrowUpSquareFill } from "react-icons/bs";
+
+const StyledFooter = styled.footer`
+  background-color: #333;
+  color: white;
+  padding: 2rem 1rem;
+  text-align: center;
+`;
+
+const FooterSmallTitleWrapper = styled.div`
+  margin-bottom: 1rem;
+`;
+
+const FooterSmallTitle = styled.h2`
+  font-size: 1.5rem;
+  margin: 0;
+`;
+
+const FooterBigLinkContainer = styled.div`
+  margin-bottom: 2rem;
+`;
+
+const FooterBigLink = styled.a`
+  font-size: 1.25rem;
+  color: white;
+  text-decoration: none;
+  position: relative;
+
+  &:hover::after {
+    content: attr(data-hover);
+    position: absolute;
+    left: 0;
+    top: 100%;
+    white-space: nowrap;
+    background: rgba(0, 0, 0, 0.8);
+    padding: 0.5rem;
+    border-radius: 0.25rem;
+  }
+`;
+
+const FooterContacts = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  margin-bottom: 2rem;
+`;
+
+const FooterContact = styled.div`
+  flex: 1;
+  min-width: 200px;
+`;
+
+const ContactHeader = styled.h3`
+  font-size: 1.25rem;
+  margin-bottom: 0.5rem;
+`;
+
+const ContactLink = styled.a`
+  font-size: 1rem;
+  color: white;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  & > svg {
+    vertical-align: middle;
+    margin-right: 0.5rem;
+  }
+`;
+
+const FooterSocials = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin-bottom: 2rem;
+
+  & > a {
+    font-size: 1.5rem;
+  }
+`;
+
+const FooterLine = styled.hr`
+  border: none;
+  border-top: 1px solid #555;
+  margin: 2rem 0;
+`;
 
 const Footer = () => {
   const toTop = () => {
@@ -32,61 +106,51 @@ const Footer = () => {
 
       <FooterBigLinkContainer>
         <FooterBigLink
-          href="mailto:cheruiyotbony@gmail.com"
+          href="#"
+          onClick={toTop}
           data-hover="Let's talk!"
         >
           Need a developer?
         </FooterBigLink>
       </FooterBigLinkContainer>
 
+      <FooterSocials>
+        <ContactLink
+          href="https://github.com/kieyp"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <SiGithub />
+        </ContactLink>
+        <ContactLink
+          href="https://www.linkedin.com/in/kipbon/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <SiLinkedin />
+        </ContactLink>
+        <ContactLink
+          href="https://twitter.com/cheruiyotbonie"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
+          <SiTwitter />
+        </ContactLink>
+      </FooterSocials>
+
       <FooterContacts>
         <FooterContact>
-          <ContactHeader>Call Me</ContactHeader>
-          <ContactLink href="tel:0720455615">0720455615</ContactLink>
-        </FooterContact>
-        <FooterContact>
-          <ContactHeader>Social</ContactHeader>{" "}
-          <FooterSocials>
-            <ContactLink
-              href="https://github.com/kieyp"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <SiGithub />
-            </ContactLink>
-            <ContactLink
-              href="https://www.linkedin.com/in/kipbon/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <SiLinkedin />
-            </ContactLink>
-            <ContactLink
-              href="https://twitter.com/cheruiyotbonie"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <SiTwitter />
-            </ContactLink>
-          </FooterSocials>
-        </FooterContact>
-        <FooterContact>
-          <ContactHeader>Mail</ContactHeader>
-          <ContactLink href="mailto:cheruiyotbony@gmail.com">
-            cheruiyotbony@gmail.com
-          </ContactLink>
+          <ContactHeader>Social</ContactHeader>
         </FooterContact>
       </FooterContacts>
 
       <FooterLine />
 
-      <ArrowToTop onClick={toTop}>
-        <BsFillArrowUpSquareFill style={{ width: "40px", color: "white" }} />
-        <span style={{ fontSize: "20px", marginLeft: "10px" }}>Go to Top</span>
-      </ArrowToTop>
-
       <div style={{ textAlign: "center", color: "#888", marginTop: "1rem" }}>
-        &copy; {year} Your Name. All Rights Reserved.
+        &copy; {year} Bon. All Rights Reserved.
       </div>
     </StyledFooter>
   );
