e8fb046465e8e130bcc0370fbbfd5db23c8bd08a
Boniface <cheruiyotbony@gmail.com>
2024-07-01 20:47:06 +0300

    done

--- src/components/Projects/projectImages.js
+++ src/components/Projects/projectImages.js
@@ -1,5 +1,5 @@
-import meesho1laptop from "../../assets/projects/meesho1laptop.png";
-import meesho1Mobile from "../../assets/projects/2of.png";
+import meesho1laptop from "../../assets/projects/SPIRITSFULLY-blog-post-website.png";
+import meesho1Mobile from "../../assets/projects/offer-banner.webp";
 import shoppersstop2Laptop from "../../assets/projects/bon1.jpg";
 import shoppersstop2Mobile from "../../assets/projects/bon1.jpg";
 import uniqlo1laptop from "../../assets/projects/uniqlo-1-laptop.png";
