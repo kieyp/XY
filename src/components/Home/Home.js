ea499e73dd0070cbf56d69ee7fdd111ebcb29c3d
Boniface <cheruiyotbony@gmail.com>
2024-07-10 19:20:39 +0300

    done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { IconContext } from "react-icons";
 import { IoIosCall } from "react-icons/io";
 import { SiLinkedin, SiGithub, SiGmail } from "react-icons/si";
-import profileImage from "../../assets/abc.jpg"; // Example profile image import
+import profileImage from "../../assets/paper-style-dynamic-lines-background_23-2149008629.jpg"; // Example profile image import
 
 const StyledHome = styled.div`
   position: relative;
